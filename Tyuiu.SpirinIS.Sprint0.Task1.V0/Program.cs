3f2fcdb4df70abcef2a76deccb4e52a6e03b41df
Anna-vormanas <annetta.vorman@gmail.com>
2026-09-16 12:17:10 +0500

    Task1

--- Tyuiu.SpirinIS.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.SpirinIS.Sprint0.Task1.V0/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tyuiu.SpirinIS.Sprint0.Task1.V0
+namespace Tyuiu.VormanAS.Sprint0.Task1.V0
 {
     internal class Program
     {
